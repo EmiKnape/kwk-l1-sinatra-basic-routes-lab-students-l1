880fe05002e582d52c2b8d865a7cb2f6b9e80528
EmiKnape <enknape@aol.com>
2018-06-12 21:33:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello World!"
 end
+  get '/name' do 
+    "My name is Emi"
+  end 
+  get '/hometown'do 
+    "My hometown is Cave Creek"
+  end 
+  get '/favorite-song' do
+    "My favorite song is Come On Eileen by Dexys Midnight Runners"
+  end
+end 
